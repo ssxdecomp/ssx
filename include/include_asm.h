7b68c7ae05fff14314402ebc0d90e16daef2a35a
modeco80 <lily.modeco80@protonmail.ch>
2025-07-01 20:39:20 -0400

    include: Remove NON_MATCHING from INCLUDE_ASM
    /shrug.

--- include/include_asm.h
+++ include/include_asm.h
@@ -3,26 +3,24 @@
 
 #if !defined(SPLAT) && !defined(M2CTX) && !defined(PERMUTER) && !defined(SKIP_ASM)
 #ifndef INCLUDE_ASM
-#define INCLUDE_ASM(FOLDER, NAME)                                       \
-    __asm__(                                                            \
-        ".section .text\n"                                              \
-        "   .set noat\n"                                                \
-        "   .set noreorder\n"                                           \
-        "   .globl\t" #NAME ".NON_MATCHING\n"                           \
-        "   .ent\t" #NAME "\n" #NAME ".NON_MATCHING:\n"                 \
-        "   .include \"asm/nonmatchings/" FOLDER "/" #NAME ".s\"\n"     \
-        "   .set reorder\n"                                             \
-        "   .set at\n"                                                  \
-        "   .end\t" #NAME                                               \
-    );
+#define INCLUDE_ASM(FOLDER, NAME)                                   \
+	__asm__(                                                        \
+		".section .text\n"                                          \
+		"   .set noat\n"                                            \
+		"   .set noreorder\n"                                       \
+		"   .globl\t" #NAME "\n"                                    \
+		"   .ent\t" #NAME "\n" #NAME ":\n"                          \
+		"   .include \"asm/nonmatchings/" FOLDER "/" #NAME ".s\"\n" \
+		"   .set reorder\n"                                         \
+		"   .set at\n"                                              \
+		"   .end\t" #NAME);
 #endif
 #ifndef INCLUDE_RODATA
-#define INCLUDE_RODATA(FOLDER, NAME)                                    \
-    __asm__(                                                            \
-        ".section .rodata\n"                                            \
-        "    .include \"asm/nonmatchings/" FOLDER "/" #NAME ".s\"\n"    \
-        ".section .text"                                                \
-    )
+#define INCLUDE_RODATA(FOLDER, NAME)                                 \
+	__asm__(                                                         \
+		".section .rodata\n"                                         \
+		"    .include \"asm/nonmatchings/" FOLDER "/" #NAME ".s\"\n" \
+		".section .text")
 #endif
 __asm__(".include \"include/macro.inc\"\n");
 #else
