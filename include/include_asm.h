7a1c6a7b996f56ee722827b52a2ed1e613bb98cc
modeco80 <lily.modeco80@protonmail.ch>
2025-09-23 00:23:26 -0400

    re-add objdiff defines to include_asm

--- include/include_asm.h
+++ include/include_asm.h
@@ -1,26 +1,24 @@
 #ifndef INCLUDE_ASM_H
 #define INCLUDE_ASM_H
 
-#if !defined(M2CTX) && !defined(PERMUTER)
+#if !defined(M2CTX) && !defined(PERMUTER) && !defined(PERMUTER) && !defined(SKIP_ASM)
 
 #ifndef INCLUDE_ASM
-#define INCLUDE_ASM(FOLDER, NAME) \
-    __asm__( \
-        ".section .text\n" \
-        "    .set noat\n" \
-        "    .set noreorder\n" \
-        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
-        "    .set reorder\n" \
-        "    .set at\n" \
-    )
+#define INCLUDE_ASM(FOLDER, NAME)                   \
+	__asm__(                                        \
+		".section .text\n"                          \
+		"    .set noat\n"                           \
+		"    .set noreorder\n"                      \
+		"    .include \"" FOLDER "/" #NAME ".s\"\n" \
+		"    .set reorder\n"                        \
+		"    .set at\n")
 #endif
 #ifndef INCLUDE_RODATA
-#define INCLUDE_RODATA(FOLDER, NAME) \
-    __asm__( \
-        ".section .rodata\n" \
-        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
-        ".section .text" \
-    )
+#define INCLUDE_RODATA(FOLDER, NAME)                \
+	__asm__(                                        \
+		".section .rodata\n"                        \
+		"    .include \"" FOLDER "/" #NAME ".s\"\n" \
+		".section .text")
 #endif
 __asm__(".include \"include/labels.inc\"\n");
 
